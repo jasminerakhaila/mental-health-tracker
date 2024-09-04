ac581aa71ff0cc679967b3c3b32fb5ebd5d1aa2f
jasminerakhaila <jrakhaila@gmail.com>
2024-09-04 13:48:26 +0700

    updated selesai

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306165774',
+        'name': 'Jasmine Rakhaila',
+        'class': 'PBP C'
     }
 
     return render(request, "main.html", context)

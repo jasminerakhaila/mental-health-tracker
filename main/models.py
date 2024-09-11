2885ae6cf99ddae6661e3d0f419cee44e537d678
jasminerakhaila <jrakhaila@gmail.com>
2024-09-11 13:20:39 +0700

    tutorial 2

--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
+import uuid  # tambahkan baris ini di paling atas
 from django.db import models
 
 # Create your models here.
 from django.db import models
 
+
 class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%b#&lny0=9om)_6&ar7@3zm-%1$xugnn)-3b*_7xw167^!obcg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jasmine-rakhaila-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jasmine-rakhaila-mentalhealthtracker.pbp.cs.ui.ac.id", "https://jasmine-rakhaila-mentalhealthtracker.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
